614d5bb8e42461c4cc11746cf5dc0ae43c8aff8d
daniperino <daniperino@gmail.com>
2023-02-05 10:00:01 -0300

    exercise day

--- cursoGuanabara/aula12Ex/ex014/script.js
+++ cursoGuanabara/aula12Ex/ex014/script.js
@@ -1,3 +1,22 @@
-//Arquivo gerado através do HTML, pela tag <script>
+//Arquivo gerado através do HTML, pela tag <scrip>
 
-alert('Olá!')
+function carregar (){
+    var msg = document.getElementById('msg')
+    var imagem = document.getElementById('imagem')
+    var dataAtual = new Date()
+    //var hora = dataAtual.getHours()
+    var hora = 13
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12){
+        //Bom Dia!
+        img.src = 'manha.png'
+    }else if (hora >=12 && hora < 18){
+        //Boa Tarde!
+        img.src = 'tarde.png'
+    }else{
+        //Boa noite!
+        img.src = 'noite.png'
+    }
+}
+
+
