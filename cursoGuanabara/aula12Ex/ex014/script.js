0f9306686d57011371b461283d94af904e2eb47e
unknown <daniperino>
2023-02-17 13:39:52 -0300

    add array of days of the week

--- cursoGuanabara/aula12Ex/ex014/script.js
+++ cursoGuanabara/aula12Ex/ex014/script.js
@@ -1,20 +1,24 @@
 //Arquivo gerado através do HTML, pela tag <scrip>
 
+weekDays = ["Domingo", "Segunda-Feira", "Tera-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"]
+// array dos dias da semana para não ficar números
 function carregar(){
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
+    const msg = document.getElementById('msg') //const pq nunca vai mudar, então não usa let
+    const img = document.getElementById('imagem')
 
-    var dataAtual = new Date()
-    //var hora = dataAtual.getHours()
-    var diaSemana = dataAtual.getDay() //pegou o dia da semana mas em número
-    var hora = 8
+    const dataAtual = new Date()
+    let hora = dataAtual.getHours()
+    let diaSemana = weekDays[dataAtual.getDay()] //coloca o array e dentro das colchetes[] coloca o comando
+    //let hora = 13
     msg.innerHTML = `Agora são ${hora} horas - ${diaSemana}`
+
+    console.log(weekDays)
 
     if (hora >= 0 && hora < 12){
         //Bom Dia!
         img.src = 'manha.png'
         document.body.style.background='#c8c5b9'
-    }else if (hora >=12 && hora < 18){
+    }else if (hora < 18){
         //Boa Tarde!
         img.src = 'tarde.png'
         document.body.style.background='#fea03c'
