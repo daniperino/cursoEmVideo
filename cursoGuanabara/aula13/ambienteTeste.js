3103855e9f1b5fee5cf71abc5fa633bad052311a
daniperino <daniperino@gmail.com>
2023-02-20 15:33:36 -0300

    while and do while

---
+++ cursoGuanabara/aula13/ambienteTeste.js
@@ -0,0 +1,16 @@
+/*console.log ('tudo bem')*/
+
+    /* testa primeiro e executa depois - WHILE
+    let fatias = 1
+
+    while (fatias < 9){
+        console.log(`Passo ${fatias}`)
+        fatias++
+    }*/
+
+/*Executa primeiro e depois testa - DO WHILE*/
+let fatias = 1
+do{
+    console.log(`Passo ${fatias}`)
+    fatias++
+}while (fatias < 9)
