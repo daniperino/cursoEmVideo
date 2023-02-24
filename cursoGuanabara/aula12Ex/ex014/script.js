12ef201bac2f363778b06734e697c6ba180cc4af
daniperino <daniperino@gmail.com>
2023-02-24 08:21:39 -0300

    add time of day

--- cursoGuanabara/aula12Ex/ex014/script.js
+++ cursoGuanabara/aula12Ex/ex014/script.js
@@ -2,28 +2,28 @@
 
 weekDays = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"]
 // array dos dias da semana para não ficar números
-function carregar(){
+function carregar(){ //as variaveis são somente do JS, criei uma const e peguei o ID no do HTML
     const msg = document.getElementById('msg') //const pq nunca vai mudar, então não usa let
     const image = document.getElementById('imagem')
-    const mdd = document.getElementById('mensagemDoDia')
+    const periodo = document.getElementById('periodoDoDia')
 
     const dataAtual = new Date()
-    //let hora = dataAtual.getHours()
-    let diaSemana = weekDays[dataAtual.getDay()] //coloca o array e dentro das colchetes[] coloca o comando
-    let hora = 8
+    let hora = dataAtual.getHours()
+    let diaSemana = weekDays[dataAtual.getDay()] //coloca o array e dentro dos colchetes[] coloca o comando
+    //let hora = 20
     msg.innerHTML = `Agora são ${hora} horas - ${diaSemana}`
 
 
     if (hora >= 0 && hora < 12){
-            mdd.innerHTML = ' Bom dia!'
+            periodo.innerHTML = 'Bom dia!'
             image.src = 'manha.png'
         document.body.style.background='#c8c5b9'
     }else if (hora < 18){
-        //Boa Tarde!
+            periodo.innerHTML = 'Boa Tarde!'
         image.src = 'tarde.png'
         document.body.style.background='#fea03c'
     }else{
-        //Boa noite!
+            periodo.innerHTML='Boa noite!'
         image.src = 'noite.png'
         document.body.style.background='#413e49'
     }
