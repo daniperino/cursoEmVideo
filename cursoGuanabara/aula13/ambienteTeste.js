f345d0c149882316f09f8d5b0a207954fe836ec1
daniperino <daniperino@gmail.com>
2023-02-24 10:27:42 -0300

    exercises FOR

--- cursoGuanabara/aula13/ambienteTeste.js
+++ cursoGuanabara/aula13/ambienteTeste.js
@@ -8,9 +8,18 @@
         fatias++
     }*/
 
-/*Executa primeiro e depois testa - DO WHILE*/
+/*Executa primeiro e depois testa - DO WHILE
 let fatias = 1
 do{
     console.log(`Passo ${fatias}`)
     fatias++
 }while (fatias < 9)
+
+let c=1
+while (c<=10){
+    console.log(c++) //console.log() é para aparecer no console
+}*/
+
+for (c=1; c<=10; c++){ //mesmo código de cima mas na estrutura do FOR
+    console.log(c)
+}
---
+++ cursoGuanabara/aula14/ambiente.js
@@ -0,0 +1,17 @@
+/*let c = 1
+while (c<=4) {
+    console.log(c)
+    c++
+}*/
+
+/*let c = 1
+do {
+    console.log(c)
+    c++
+}while (c<=3)*/
+
+console.log('Vai começar...')
+for (c=1; c<=3; c++){
+    console.log(c)
+}
+console.log('Fim!')
