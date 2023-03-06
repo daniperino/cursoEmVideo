adca0aafa67ef08dfbcfd0994f5a9286b98e9e9f
daniperino <daniperino@gmail.com>
2023-03-06 17:09:56 -0300

    Add exercise 16 of lesson 14

---
+++ cursoGuanabara/aula14ex/ex016/script.js
@@ -0,0 +1,16 @@
+//Arquivo gerado através do HTML, pela tag <script>
+
+function contar(){
+    let formInicio = document.getElementById('numInicio')
+    let formFim = document.getElementById('numFim')
+    let formPasso = document.getElementById('numPasso')
+    let resul = document.getElementById('preparar')
+    if (formInicio.value.length == '' || formFim.value.length == '' || formPasso.value.length == ''){
+        alert('[ERRO] Formulário não pode estar vazio, preencha!!!')
+
+    }
+
+
+
+
+}
