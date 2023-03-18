54ad9a1fb5486cac58381ced39ca6c4a7939f231
daniperino <daniperino@gmail.com>
2023-03-18 12:17:20 -0300

    more exercises

--- cursoGuanabara/aula14ex/ex016/script.js
+++ cursoGuanabara/aula14ex/ex016/script.js
@@ -4,22 +4,30 @@ function contar(){
     let formInicio = document.getElementById('numInicio')
     let formFim = document.getElementById('numFim')
     let formPasso = document.getElementById('numPasso')
-    let resul = document.getElementById('preparar')
-    if (formInicio.value.length == '' || formFim.value.length == '' || formPasso.value.length == ''){
-        alert('[ERRO] Formulário não pode estar vazio, preencha!!!')
-   }else{
-        resul.innerHTML = 'Contando...'
+    let res = document.getElementById('resul')
+    if (formInicio.value.length == 0 || formFim.value.length == 0 || formPasso.value.length == 0){
+        //alert('[ERRO] Formulário não pode estar vazio, preencha!!!')
+        res.innerHTML = 'Impossível contar, preencha o formulário!!!'
+   } else {
+        res.innerHTML = 'Contando... <br> '
         let i = Number(formInicio.value)
         let f = Number(formFim.value)
         let p = Number(formPasso.value)
-
-        for(let c = i; c <= f; +=c){
-            resul.innerHTML = `${c}`
+        if(p <= 0){
+            alert('Passo invalido!!! Considerando passo = 1')
+            p = 1
         }
+        if (i < f) { //contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` \u{1F449} ${c} ` // Unicode emoji list
+            }
+        }else{//contagem decrescente
+                for(let c = i; c >= f; c -= p){
+                    res.innerHTML += ` \u{1F449} ${c} `
+            }
 
+        }
+        resul.innerHTML += `\u{274C}`
     }
-
-
-
 
 }
