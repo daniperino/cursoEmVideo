65d835c23c0842a643d418899e0171650f0294ea
daniperino <daniperino@gmail.com>
2023-05-13 10:54:50 -0300

    multiplication table exercise

--- cursoGuanabara/aula14ex/ex017/script.js
+++ cursoGuanabara/aula14ex/ex017/script.js
@@ -1,3 +1,33 @@
 //Arquivo gerado através do HTML, pela tag <script>
 
-alert('Olá!')
+function tabuada(){
+    let num = document.getElementById('txtNum')
+    let tab = document.getElementById('selTab')
+
+    if(num.value.length == 0){
+        alert('Por favor, digite um número!') 
+    }else{
+        let n = Number(num.value) //converter os valores
+        tab.innerText = ''
+        for(let c = 1; c <=10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+        
+        /* OU usando o while
+        let c = 1
+
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }*/
+        
+    }
+        
+        
+    
+}
